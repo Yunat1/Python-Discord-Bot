36f98fbbf44acdd7f7b5ddcf2fd7aec6000b8114
Michael Reißmüller <michael.reissmueller@uni-bielefeld.de>
2021-05-11 18:28:08 +0200

    Implemented Parallelization with event loop task

--- cogs/rolePing.py
+++ cogs/rolePing.py
@@ -3,10 +3,28 @@ import os
 import asyncio
 from discord.ext import commands
 
+
 class RolePing(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
+    #task for event loop
+    async def private_message(self, ctx, message, member, content):
+
+        def message_check(message):
+            return message.channel.type == discord.ChannelType.private and message.author == member
+
+        await member.send(content + " - von " + message.author.name)
+        try: 
+            #thread for each user to ask at the same time
+            response = await self.client.wait_for('message', timeout= 60.0, check=message_check)
+            if response:
+                await ctx.send(response.content + " - " + member.name)
+        except asyncio.TimeoutError:
+            await ctx.send("Keine Antwort - " + member.name)
+            await member.send("Ja, egal hat sich erledigt")
+
 
     #Extract content from message for wikipedia Search
     @commands.command(name='game')
@@ -16,9 +34,6 @@ class RolePing(commands.Cog):
         message = ctx.message
         content = ctx.message.content[6:]
 
-        def message_check(message):
-            return message.channel.type == discord.ChannelType.private and message.author == member
-
         #extract game from message and remove ping
         if '@everyone' in content:
             content = content.replace('@everyone ', '')
@@ -27,33 +42,16 @@ class RolePing(commands.Cog):
             content = content[:index] + content [index + 23:]
 
         #Dm users with pinged role
+        threads = list()
         if message.mention_everyone:
             members = message.guild.members
         else:
             role_mention = message.role_mentions
             members = role_mention[0].members
         for member in members:
-            if member != message.author and member != self.client.user:
-                await member.send(content + " - von " + message.author.name)
-                try: 
-                    #thread for each user to ask at the same time
-                    response = await self.client.wait_for('message', timeout= 60.0, check=message_check)
-                    if response:
-                        await ctx.send(response.content + " - " + member.name)
-                except asyncio.TimeoutError:
-                    await ctx.send("Keine Antwort - " + member.name)
-                    await member.send("Ja, egal hat sich erledigt")
+            if member != self.client.user:
+                #using event loop with task instead of threats
+                asyncio.create_task(self.private_message(ctx, message, member, content))
 
-                    #write to file and send text file at the end   
-               
-        '''    
-        test = message.role_mentions
-        for role in message.guild.roles:
-            if test[0].id == role.id:
-                for member in role.members:
-                    if member != self.client.user:
-                        await member.send("Hallo")
-        '''
-    
 def setup(client):
     client.add_cog(RolePing(client))
