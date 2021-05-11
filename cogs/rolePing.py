cd09a1040beddf69511ecb18417eab7dfe41a95a
Michael Reißmüller <michael.reissmueller@uni-bielefeld.de>
2021-05-11 18:58:59 +0200

    Modified to send text file with answers from all users

--- cogs/rolePing.py
+++ cogs/rolePing.py
@@ -9,23 +9,6 @@ class RolePing(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    #task for event loop
-    async def private_message(self, ctx, message, member, content):
-
-        def message_check(message):
-            return message.channel.type == discord.ChannelType.private and message.author == member
-
-        await member.send(content + " - von " + message.author.name)
-        try: 
-            #thread for each user to ask at the same time
-            response = await self.client.wait_for('message', timeout= 60.0, check=message_check)
-            if response:
-                await ctx.send(response.content + " - " + member.name)
-        except asyncio.TimeoutError:
-            await ctx.send("Keine Antwort - " + member.name)
-            await member.send("Ja, egal hat sich erledigt")
-
-
     #Extract content from message for wikipedia Search
     @commands.command(name='game')
     async def game(self, ctx):
@@ -43,15 +26,35 @@ class RolePing(commands.Cog):
 
         #Dm users with pinged role
         threads = list()
+        f = open("answers.txt", "w", encoding='utf-8')
         if message.mention_everyone:
             members = message.guild.members
         else:
             role_mention = message.role_mentions
             members = role_mention[0].members
-        for member in members:
-            if member != self.client.user:
-                #using event loop with task instead of threats
-                asyncio.create_task(self.private_message(ctx, message, member, content))
+        
+        #task for event loop
+        async def message_member(member, ctx, message, content, answers):
+
+            def message_check(message):
+                return message.channel.type == discord.ChannelType.private and message.author == member
+
+            if member != message.author and member != self.client.user:
+                await member.send(content + " - von " + message.author.name)
+                try: 
+                    response = await self.client.wait_for('message', timeout= 60.0, check=message_check)
+                    if response:
+                        answers.write(response.content + " - " + member.name + '\n')
+                except asyncio.TimeoutError:
+                    answers.write("Keine Antwort - " + member.name)
+                    await member.send("Ja, egal hat sich erledigt")
+        
+        #Use Event Loop instead of threats
+        #wait for every task to finish before continuing
+        await asyncio.wait([asyncio.ensure_future(message_member(member, ctx, message, content, f)) for member in members])
+        f.close()
+        await ctx.send(file=discord.File("answers.txt"))
+        os.remove("answers.txt")
 
 def setup(client):
     client.add_cog(RolePing(client))
